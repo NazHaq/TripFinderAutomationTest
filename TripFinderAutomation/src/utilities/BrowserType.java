fbbb3c6ea693a672b4c7fcd1e75faf89ff531436
NazHaq <nazmul_02eee@yahoo.com>
2016-12-16 18:14:52 +0800

    Added Comment

--- TripFinderAutomation/src/utilities/BrowserType.java
+++ TripFinderAutomation/src/utilities/BrowserType.java
@@ -10,13 +10,11 @@ public class BrowserType {
 
          // Set Browser
          WebDriver driver = null;
-
-        	 System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\ChromeDriver\\chromedriver.exe");
-             driver = new ChromeDriver();        
+         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\ChromeDriver\\chromedriver.exe");
+         driver = new ChromeDriver();        
           // Navigate to URL
           driver.get(Constant.URL);
           driver.manage().window().maximize();
-
           return driver;
 
     }
